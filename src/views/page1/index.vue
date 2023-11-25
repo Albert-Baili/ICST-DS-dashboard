136d21a9cda6d333903418100e62f1e26f615ccb
5hell-Yu <584475195@qq.com>
2023-11-25 11:11:36 +0800

    yu

--- src/views/page1/index.vue
+++ src/views/page1/index.vue
@@ -1,17 +1,28 @@
 <template>
-<div>
+  <div>
     <ItemWrap class="contetn_left-top contetn_lr-item" title="设备资产监测">
-
-    <LeftTop />
+      <LeftTop />
     </ItemWrap>
-</div>
+    <ItemWrap class="contetn_left-center contetn_lr-item" title="设备提醒">
+      <LeftBottom />
+    </ItemWrap>
+  </div>
 </template>
 
 <script>
 import LeftTop from './left-top.vue'
+import LeftBottom from './left-bottom.vue'
 export default {
   components: {
     LeftTop,
+    LeftBottom,
   }
 }
 </script>
+<style>
+.contetn_lr-item {
+  height: 310px;
+  padding: 0 10px 16px 10px;
+  width: 30%;
+}
+</style>
