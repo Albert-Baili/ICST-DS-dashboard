27ffe17dc1b5cf555181114115c97c7ebf69d80f
5hell-Yu <584475195@qq.com>
2024-01-05 21:42:24 +0800

    assettype test by yu

--- src/views/page1/index.vue
+++ src/views/page1/index.vue
@@ -6,16 +6,21 @@
     <ItemWrap class="contetn_left-center contetn_lr-item1" title="设备提醒">
       <LeftBottom />
     </ItemWrap>
+      <ItemWrap class="contetn_left-bottom contetn_lr-item1" title="设备类型分布" style="padding: 0 10px 16px 10px">
+        <LeftCenter />
+      </ItemWrap>
   </div>
 </template>
 
 <script>
 import LeftTop from './left-top.vue'
 import LeftBottom from './left-bottom.vue'
+import LeftCenter from './left-center.vue'
 export default {
   components: {
     LeftTop,
     LeftBottom,
+    LeftCenter,
   }
 }
 </script>
@@ -25,4 +30,13 @@ export default {
   padding: 0 10px 16px 10px;
   width: 30%;
 }
+.contetn_left,
+.contetn_right {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  position: relative;
+  width: 30%;
+}
+
 </style>
